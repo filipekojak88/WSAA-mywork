051ca4c8f5aa2957e95fb3956232714ae614ac34
Filipe Carvalho <filipeferc88@gmail.com>
2025-03-05 01:20:36 +0000

    corrected mistake on apiKey

--- lab5.2-html2pdf.py
+++ lab5.2-html2pdf.py
@@ -1,20 +1,19 @@
 import requests
 import urllib.parse
-from config import config as cfg
+from config import apikeys as cfg
 
-target_url = "https://en.wikipedia.org"
+target_url = "https://en.wikipedia.org/wiki/Main_Page"
 
 
-api_key = cfg['htmltopdfkey']   
+apikey = cfg['htmltopdfkey']   
+#print (api_key)
 
 api_url = "https://api.html2pdf.app/v1/generate"
 
-params = {
-    'html': target_url,
-    'apikey': api_key
-}
+params = {'html': target_url,'apiKey': apikey}
 parsed_params = urllib.parse.urlencode(params)
 print(parsed_params)
+
 request_url = api_url + "?" + parsed_params
 print(request_url)
 
@@ -24,6 +23,6 @@ print(response.text)  # Print error details
 
 
 result = response.content
-with open ("document.pdf", "wb") as f:
-    f.write(result)
+with open ("document.pdf", "wb") as handler:
+    handler.write(result)
     print("File saved")
